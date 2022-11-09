5fd2c706c7fc524632d4553ee94316bb8ac6763c
jeffpatriota <jefferson.patriota@outlook.com>
2022-11-09 17:10:54 -0300

    exibindo feed(nome, curtida(s) e comentario(s))

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
+import Feed from './components/Feed';
 
 
 class App extends Component {
@@ -6,34 +8,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: false
+      feed: [
+        { id: 1, username: 'Jeff', curtidas: 10, comentarios: 2 },
+        { id: 2, username: 'Jessy', curtidas: 25, comentarios: 12 },
+        { id: 3, username: 'Silvia', curtidas: 13, comentarios: 9 },
+        { id: 4, username: 'Sirius', curtidas: 1, comentarios: 0 },
+      ]
     };
-    this.sair = this.sair.bind(this);
-    this.entrar = this.entrar.bind(this);
 
-  }
 
-  sair() {
-    this.setState({ status: false });
-  }
-
-  entrar() {
-    this.setState({ status: true });
   }
 
   render() {
     return (
       <div>
-        {this.state.status ?
-          <div>
-            <h2>Bem-vindo ao sistema</h2>
-            <button onClick={this.sair}>Sair</button>
-          </div> :
-          <div>
-            <h2>Olá visitante, faça o login</h2>
-            <button onClick={this.entrar}>Entrar no sistema</button>
-          </div>
+        {this.state.feed.map((item) => {
+          return (
+            <Feed id={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios} />
+          );
         }
+        )}
 
       </div>
     );
